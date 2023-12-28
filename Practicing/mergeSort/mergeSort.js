31ab435115b79d9fe37d7fd02cfaf0cc875edeb7
Vanessa <zdperfectx@gmail.com>
2023-12-28 15:30:40 -0600

    added code into mergeSort

--- Practicing/mergeSort/mergeSort.js
+++ Practicing/mergeSort/mergeSort.js
@@ -1,17 +1,17 @@
 function mergeSort(compare, elements) {
-    if (Array.isArray(elements)) {
-        if (elements.length <= 1) {
-            return elements;
-        }
-        const middle = Math.floor(elements.length / 2);
-        const leftElements = elements.slice(0, middle);
-        const rightElements = elements.slice(middle);
-        const leftElementsSorted = mergeSort(compare, leftElements);
-        const rightElementsSorted = mergeSort(compare, rightElements);
-
-        return merge(compare, leftElementsSorted, rightElementsSorted);
+  if (Array.isArray(elements)) {
+    if (elements.length <= 1) {
+      return elements;
     }
-    return elements;
+    const middle = Math.floor(elements.length / 2);
+    const leftElements = elements.slice(0, middle);
+    const rightElements = elements.slice(middle);
+    const leftElementsSorted = mergeSort(compare, leftElements);
+    const rightElementsSorted = mergeSort(compare, rightElements);
+
+    return merge(compare, leftElementsSorted, rightElementsSorted);
+  }
+  return elements;
 }
 
 /**
@@ -28,23 +28,23 @@ function mergeSort(compare, elements) {
  */
 
 function merge(compare, left, right) {
-    const sorted = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-        const comparison = compare(left[leftIndex], right[rightIndex]);
+  const sorted = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    const comparison = compare(left[leftIndex], right[rightIndex]);
 
-        if (comparison <= 0) {
-            sorted.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            sorted.push(right[rightIndex]);
-            rightIndex++;
-        }
+    if (comparison <= 0) {
+      sorted.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      sorted.push(right[rightIndex]);
+      rightIndex++;
     }
-    return sorted.concat(
-        leftIndex < left.length ? left.slice(leftIndex) : right.slice(rightIndex)
-    );
+  }
+  return sorted.concat(
+    leftIndex < left.length ? left.slice(leftIndex) : right.slice(rightIndex)
+  );
 }
 
 module.exports = mergeSort;
